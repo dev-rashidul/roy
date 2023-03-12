e90b5d035c079a3679a8b3217a53748d3e97d263
dev-rashidul <rashidul.contact@gmail.com>
2023-03-12 14:24:35 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 // JavaScript Code for New Collection Slider
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  slidesPerView: 3,
+  spaceBetween: 30,
+  centeredSlides: true,
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
